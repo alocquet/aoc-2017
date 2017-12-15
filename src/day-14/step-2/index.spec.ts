96d3512108cfd9861cdef12b8300f03d9769c503
Locquet Arnaud <arnaud.locquet@gfi.fr>
2017-12-15 15:18:44 +0100

    perf

--- src/day-14/step-2/index.spec.ts
+++ src/day-14/step-2/index.spec.ts
@@ -5,14 +5,10 @@ import { Day14Step2 } from '.';
 describe('day-14-s2', () => {
     let day = new Day14Step2();
 
-    it('In the example, 1242 regions are present.', function(done) {
-        this.timeout(20000);
+    it('In the example, 1242 regions are present.', () => {
         expect(day.execute('flqrgnkx')).to.be.equal(1242);
-        done();
     });
-    it('oundnydw give result 1164.', function(done) {
-        this.timeout(20000);
+    it('oundnydw give result 1164.', () => {
         expect(day.execute('oundnydw')).to.be.equal(1164);
-        done();
     });
 });
--- src/day-14/step-2/index.ts
+++ src/day-14/step-2/index.ts
@@ -4,44 +4,51 @@ export class Day14Step2 extends Day14 {
 
     public execute(input: string): number {
         let disk = this.getDisk(input);
-        let groups: string[][] = [];
+        let nodes = new Map<string, number>();
+        let groups = new Map<number, string[]>();
+        let idGroup = 0;
         for (let y = 0; y < disk.length; y++) {
             for (let x = 0; x < disk[y].length; x++) {
                 let cell = disk[y][x];
                 if (cell === '1') {
-                    let inGroups = this.getGroupIds(groups, x, y);
-                    if (inGroups.length === 0) {
+                    let inGroups = this.getGroupIds(nodes, x, y);
+                    if (inGroups.size === 0) {
                         // no groups : create a new groupe
-                        groups.push([this.stringRepresentation(x, y)]);
+                        groups.set(idGroup, [this.stringRepresentation(x, y)]);
+                        nodes.set(this.stringRepresentation(x, y), idGroup++);
                     } else {
-                        let keepGroupId = inGroups.pop();
+                        let keepGroupId;
                         // join groups
                         for (let groupId of inGroups) {
-                            [].push.apply(groups[keepGroupId], groups[groupId]);
-                            groups.splice(groupId, 1);
+                            if (keepGroupId === undefined) {
+                                // keep first id to merge others groups in
+                                keepGroupId = groupId;
+                                continue;
+                            }
+                            // merge group in first group
+                            for (let node of groups.get(groupId)) {
+                                nodes.set(node, keepGroupId);
+                            }
+                            [].push.apply(groups.get(keepGroupId), groups.get(groupId));
+                            groups.delete(groupId);
                         }
                         // add x,y to group
-                        groups[keepGroupId].push(this.stringRepresentation(x, y));
+                        nodes.set(this.stringRepresentation(x, y), keepGroupId);
+                        groups.get(keepGroupId).push(this.stringRepresentation(x, y));
                     }
                 }
             }
         }
-        return groups.length;
+        return groups.size;
     }
 
     /**
      * get ids of groups where x,y is. ids are sorted desc.
      */
-    public getGroupIds(groups: string[][], x: number, y: number): number[] {
-        let result = [];
-        for (let groupId = 0; groupId < groups.length; groupId++) {
-            let neightboorInGroup = this.getNeightboors(x, y)
-                .some((neightboor) => groups[groupId].indexOf(neightboor) !== -1);
-            if (neightboorInGroup) {
-                result.unshift(groupId);
-            }
-        }
-        return result;
+    public getGroupIds(nodes: Map<string, number>, x: number, y: number): Set<number> {
+        return new Set(this.getNeightboors(x, y)
+            .filter((neightboor) => nodes.has(neightboor))
+            .map((neightboor) => nodes.get(neightboor)));
     }
 
     public getNeightboors(x: number, y: number): string[] {
