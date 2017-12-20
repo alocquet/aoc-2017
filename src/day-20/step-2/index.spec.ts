ba55937cdb41b3b249aabd266be4ac0227362e3d
Locquet Arnaud <arnaud.locquet@gfi.fr>
2017-12-20 08:58:04 +0100

    day 20 step 2

--- src/day-20/step-2/index.spec.ts
+++ src/day-20/step-2/index.spec.ts
@@ -5,4 +5,17 @@ import { Day20Step2 } from '.';
 describe('day-20-s2', () => {
     let day = new Day20Step2();
 
+    it('In the example, there is only one particle after collisions.', () => {
+        expect(day.execute(
+            `p=<-6,0,0>, v=< 3,0,0>, a=< 0,0,0>
+p=<-4,0,0>, v=< 2,0,0>, a=< 0,0,0>
+p=<-2,0,0>, v=< 1,0,0>, a=< 0,0,0>
+p=< 3,0,0>, v=<-1,0,0>, a=< 0,0,0>`)).to.be.equal(1);
+    });
+    it('With the input, there are 648 particles after collisions.', (done) => {
+        day.run('assets/day-20/input.txt').subscribe((result) => {
+            expect(result).to.be.equal(648);
+            done();
+        });
+    });
 });
--- src/day-20/step-2/index.ts
+++ src/day-20/step-2/index.ts
@@ -1,9 +1,34 @@
-import { Day20 } from '..';
+import { D20Particle, Day20 } from '..';
 
 export class Day20Step2 extends Day20 {
 
     public execute(input: string): number {
-        return 0;
+        let particles = this.parse(input);
+        let ticksBeforeExit = 200;
+        while (ticksBeforeExit > 0) {
+            particles.forEach((particle) => particle.nextState());
+
+            let particlesMap = new Map<string, D20Particle>();
+            let collision = new Set<string>();
+            // remove collisions
+            for (let particle of particles) {
+                let representation = particle.position.toString();
+                if (particlesMap.has(representation)) {
+                    collision.add(representation);
+                    particlesMap.delete(representation);
+                } else if (!collision.has(representation)) {
+                    particlesMap.set(representation, particle);
+                }
+            }
+            // update counter
+            if (particlesMap.size === particles.length) {
+                ticksBeforeExit--;
+            } else {
+                ticksBeforeExit = 200;
+                particles = [...particlesMap.values()];
+            }
+        }
+        return particles.length;
     }
 
 }
